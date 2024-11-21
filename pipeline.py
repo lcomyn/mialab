fixed the overflow error in our metrics

pipeline.py
@@ -74,8 +74,8 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
 
     warnings.warn('Random forest parameters not properly set.')
     forest = sk_ensemble.RandomForestClassifier(max_features=images[0].feature_matrix[0].shape[1],
-                                                n_estimators=30,
-                                                max_depth=40)
+                                                n_estimators=60,
+                                                max_depth=50)
 
     start_time = timeit.default_timer()
     forest.fit(data_train, labels_train)
@@ -117,7 +117,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
         print(' Time elapsed:', timeit.default_timer() - start_time, 's')
         
         # convert prediction and probabilities back to SimpleITK images
-        image_prediction = conversion.NumpySimpleITKImageBridge.convert(predictions.astype(np.uint8),
+        image_prediction = conversion.NumpySimpleITKImageBridge.convert(predictions.astype(np.float64),
                                                                         img.image_properties)
         image_probabilities = conversion.NumpySimpleITKImageBridge.convert(probabilities, img.image_properties)
 